b9c8bc96fcd8a5e56b47f5c66bb72e579ad75128
Satwik Ram <satwikram29@gmail.com>
2022-12-06 22:21:24 -0500

    model loading

--- predict/apps.py
+++ predict/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
-
+import tensorflow as tf
 
 class PredictConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'predict'
+
+    mpath = "models"
+
+    model = tf.keras.models.load_model(mpath)
