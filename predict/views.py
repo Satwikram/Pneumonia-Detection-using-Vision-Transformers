0a3c5ddc79d2568fe2546c02af5186fa8986ffdc
Satwik Ram <satwikram29@gmail.com>
2022-12-06 20:42:14 -0500

    api

--- predict/views.py
+++ predict/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+import tempfile
+from pathlib import Path
+
+import cv2
 
 # Create your views here.
+
+class PredictionAPIView(APIView):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+    def post(self, request) -> Response:
+
+        image = request.FILES['file']
+
+        filename = str(image.name)
+
+        temp = tempfile.TemporaryDirectory(
+        prefix="pneumonia_")  
+
+        temp_dir = Path(temp.name)
+        
+        uFile = temp_dir / filename
+
+        with open(uFile, "wb+") as f:
+            f.write(image.read())
+
+            image = cv2.imread(uFile, cv2.IMREAD_GRAYSCALE)
+
+            print(image)
+
+        return "Success"
